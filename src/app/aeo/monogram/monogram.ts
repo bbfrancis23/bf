de1e970f8a151cf992c2d077e0d20fa6c413c0d2
Brian Francis <bbfrancis23@gmail.com>
2018-03-15 18:33:12 -0600

    Update AeoSideDrawer

---
+++ src/app/aeo/monogram/monogram.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'aeo-monogram',
+  template: ``
+})
+export class Monogram {
+
+}
--- src/app/aeo/side-drawer/side-drawer.ts
+++ src/app/aeo/side-drawer/side-drawer.ts
@@ -2,22 +2,26 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'aeo-side-drawer',
-  templateUrl: './side-drawer.html',
+  template: `
+  <div class="aeo-sidedrawer-container" [ngClass]="{'closed': closed}">
+    <button class="aeo-sidedrawer-control" mat-fab (click)="closed = !closed" ><mat-icon>{{closed ? 'menu' : 'close'}}</mat-icon></button>
+    <ng-content></ng-content>
+  </div>`,
   styleUrls: ['./side-drawer.scss']
 })
 export class AeoSideDrawer implements OnInit {
 
-  @Input() width: number;
+  //@Input() width: number;
   closed = true;
-  _left: number;
+  //_left: number;
   constructor() { }
 
   ngOnInit() {
   }
 
-  get left() {
-    return this.closed ? -Math.abs(this.width) : 0;
-  }
+  //get left() {
+  //  return this.closed ? -Math.abs(this.width) : 0;
+  //  }
 
 
 }
