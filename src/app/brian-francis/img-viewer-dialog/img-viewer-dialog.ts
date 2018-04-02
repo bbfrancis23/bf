97817e33b327ae52693234fb2bc94e8084f1000c
Brian Francis <bbfrancis23@gmail.com>
2018-04-02 14:57:44 -0700

    final rough

---
+++ src/app/brian-francis/img-viewer-dialog/img-viewer-dialog.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+
+@Component({
+  selector: 'img-viewer-dialog',
+  template: `Hello`
+})
+export class ImgViewerDialog {
+  constructor(public dialogRef: MatDialogRef<ImgViewerDialog>) { }
+}
